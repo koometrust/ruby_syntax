785288fbdd74abab046a2841c7cb408106c9adea
koometrust <judekoome360@gmail.com>
2024-02-27 22:23:21 +0300

    loops

---
+++ for_loop.rb
@@ -0,0 +1,18 @@
+#for num in 0..10
+ #   puts num
+#end
+
+#5.times do |index|
+#   puts index * index
+#end
+
+nice_nums = [4, 5, 4, "jessy", 7, 5, 45.0]
+
+for nice_num in nice_nums
+    puts nice_num
+end
+puts "\n"
+nice_nums.each do |nice_num|
+  puts nice_num
+end
+
---
+++ while_loop.rb
@@ -0,0 +1,6 @@
+ num = 1
+
+ while num <= 7
+    puts num
+    num+=1
+ end
